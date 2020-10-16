5e05003c24f4b8f98bdbc8c0cdad38a4f5829f59
sinhvien <sinhvien>
2020-10-16 10:29:53 +0700

    hang ngu

---
+++ TMĐT2/TMĐT2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TMĐT2.Startup))]
+namespace TMĐT2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
